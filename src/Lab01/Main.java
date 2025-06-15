9cfe03c2959eb8c822fc6bb8a8c0f2fe5d2406b0
Anatolii Tupikov <atupikov775@gmail.com>
2025-06-15 13:01:29 +0200

    number to double

--- src/Lab01/Main.java
+++ src/Lab01/Main.java
@@ -3,16 +3,20 @@ package Lab01;
 import java.util.Scanner;
 
 public class Main {
-    public  static void main (String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Я\nхорошо\nзнаю\nJava.");
 
         double dresult = (46 + 10) * (10d / 3d);
         int iresult = 29 * 4 * (-15);
-        System.out.println( dresult + " " + iresult);
+        System.out.println(dresult + " " + iresult);
 
-        double result = 3.6 * 4.1 * 5.9;
+        int number = 10500;
+        double result = (double) (number / 10) / 10;
         System.out.println(result);
+
+        result = 3.6 * 4.1 * 5.9;
+        System.out.println(result);
+
 
         var scanner = new Scanner(System.in);
 
@@ -23,10 +27,7 @@ public class Main {
 
         b = scanner.nextInt();
         String answ;
-        if (b % 2 == 0)
-        {
-            answ = b > 100 ? "Выход за пределы диапазона" : "Четное";
-        }
+        if (b % 2 == 0) answ = b > 100 ? "Выход за пределы диапазона" : "Четное";
         else answ = "Нечетное";
         System.out.println(answ);
     }
