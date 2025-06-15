6974ecdc3251c7b195667a2d98678e7bc68aa714
Anatolii Tupikov <atupikov775@gmail.com>
2025-06-15 13:38:47 +0200

    Lab02 final fix

--- src/Lab02/Calculator.java
+++ src/Lab02/Calculator.java
@@ -1,62 +1,53 @@
 package Lab02;
 
 public class Calculator {
-    public double Summ (double first, double second)
-    {
-        return first + second;
-    }
-    public int Summ (int first, int second)
-    {
-        return first + second;
-    }
-    public long Summ (long first, long second)
-    {
+    public double summ(double first, double second) {
         return first + second;
     }
 
-    public double Division (double divisible, double quotient)
-    {
+    public int summ(int first, int second) {
+        return first + second;
+    }
+
+    public long summ(long first, long second) {
+        return first + second;
+    }
+
+    public double division(double divisible, double quotient) {
         return divisible / quotient;
     }
-    public int Division (int divisible, int quotient)
-    {
-        return divisible / quotient;
-    }
-    public long Division (long divisible, long quotient)
-    {
-        return divisible / quotient;
+
+    public double division(int divisible, int quotient) {
+        return (double) divisible / (double) quotient;
     }
 
-    public double Multi (double first, double second)
-    {
-        return  first * second;
+    public double division(long divisible, long quotient) {
+        return (double) divisible / (double) quotient;
     }
 
-    public int Multi (int first, int second)
-    {
-        return  first * second;
+    public double multi(double first, double second) {
+        return first * second;
     }
 
-    public long Multi (long first, long second)
-    {
-        return  first * second;
+    public int multi(int first, int second) {
+        return first * second;
     }
 
-    public double Diff (double reduced, double subtractible)
-    {
-        return  reduced - subtractible;
+    public long multi(long first, long second) {
+        return first * second;
     }
 
-    public int Diff (int reduced, int subtractible)
-    {
-        return  reduced - subtractible;
-    }
-    public long Diff (long reduced, long subtractible)
-    {
-        return  reduced - subtractible;
+    public double diff(double reduced, double subtractible) {
+        return reduced - subtractible;
     }
 
+    public int diff(int reduced, int subtractible) {
+        return reduced - subtractible;
+    }
 
+    public long diff(long reduced, long subtractible) {
+        return reduced - subtractible;
+    }
 
 
 }
--- src/Lab02/PersonalComputer.java
+++ src/Lab02/PersonalComputer.java
@@ -1,34 +1,74 @@
 package Lab02;
 
 
-
 public class PersonalComputer {
 
-    private Processor CPU;
-    public String GPU;
-    public int FansCount;
-    public  String Motherboard;
-    public  String CaseColor;
+    private Processor cpu;
+    private String gpu;
+    private int fansCount;
+    private String motherboard;
+    private String caseColor;
 
-    public PersonalComputer(){}
-
-    public PersonalComputer(Processor cpu, String motherboard, String gpu, int fans, String color)
-    {
-        CPU = cpu;
-        Motherboard = motherboard;
-        GPU = gpu;
-        FansCount = fans;
-        CaseColor = color;
+    public PersonalComputer() {
     }
 
-    public Processor getProcessor()
-    {
-        return this.CPU;
+    public PersonalComputer(Processor cpu, String motherboard, String gpu, int fans, String color) {
+        this.cpu = cpu;
+        this.motherboard = motherboard;
+        this.gpu = gpu;
+        fansCount = fans;
+        caseColor = color;
     }
 
-    public void setProcessor(Processor value)
-    {
-        this.CPU = value;
+    public Processor getProcessor() {
+        return this.cpu;
+    }
+
+    public void setProcessor(Processor value) {
+        this.cpu = value;
+    }
+
+    public String getGPU() {
+        return this.gpu;
+    }
+
+    public void setGPU(String value) {
+        this.gpu = value;
+    }
+
+    public int getFansCount() {
+        return this.fansCount;
+    }
+
+    public void setFansCount(int value) {
+        this.fansCount = value;
+    }
+
+    public String getMotherboard() {
+        return this.motherboard;
+    }
+
+    public void setMotherboard(String value) {
+        this.motherboard = value;
+    }
+
+    public String getCaseColor() {
+        return this.caseColor;
+    }
+
+    public void setCaseColor(String value) {
+        this.caseColor = value;
+    }
+
+    @Override
+    public String toString() {
+        String str = String.format("PC:\n" + "\t Processor: %s\n" + "\t Motherboard: %s\n" + "\t Video processor: %s\n" + "\t Case color: %s\n" + "\t Fans count: %d\n",
+                cpu.getDescription(),
+                motherboard,
+                gpu,
+                caseColor,
+                fansCount);
+        return str;
     }
 
 
